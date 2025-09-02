355e43d3991b787d0334d990ddf2c897747953bb
Israel Vitzman <iv058.l.v@gmail.com>
2025-09-02 17:38:50 +0300

    .

--- front-ridlle/src/components/menu.tsx
+++ front-ridlle/src/components/menu.tsx
@@ -1,8 +1,16 @@
-export function Menu(){
-    return(
-        <div>
-            <h1>ברוכים הבאים-למשחק חידות</h1>
-            <h3>תפריט</h3>
-        </div>
+import { Link } from "react-router-dom";
+export function Menu() {
+    return (
+        <nav>
+            <ul>
+                <li><Link to="/SwohRiddle">Show All Riddles</Link></li>
+                <li><Link to="/login">Login</Link></li>
+                <li><Link to="/BestPlayer">Best Player</Link></li>
+                <li><Link to="/BestPlayers">Best Players</Link></li>
+                <li><Link to="/DeleteRiddle">Delete Riddle</Link></li>
+                <li><Link to="/UpdateRiddle">Update Riddle</Link></li>
+            </ul>
+        </nav>
+
     )
 }
