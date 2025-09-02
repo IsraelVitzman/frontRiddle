d68c97e25c65de7fbcd50d30717a63d00cbee249
Israel Vitzman <iv058.l.v@gmail.com>
2025-09-02 22:23:33 +0300

    .

--- front-ridlle/src/components/play.tsx
+++ front-ridlle/src/components/play.tsx
@@ -1,11 +1,11 @@
 import { useState, useEffect } from "react";
-import { time, addTimeForAllRidders, addSecondsForQuestion, returnTime } from "./time.tsx";
+import { time, addTimeForAllRidders,  returnTime } from "./time.tsx";
 
 export default function Play() {
   const [riddles, setRiddles] = useState<any[]>([]);
-  const [timeAll, setTimeAll] = useState<number | null>(null);
+  
 
-  // טוען את החידות מהשרת
+  
   useEffect(() => {
     const loadRiddles = async () => {
       try {
@@ -19,14 +19,14 @@ export default function Play() {
     loadRiddles();
   }, []);
 
-  // פונקציה שמפעילה את הלוגיקה של חידה
+  
   const handleRiddle = async (riddle: any) => {
     const startTime = time();
-    // כאן אפשר לקרוא לפונקציות אחרות שלך
+    
     console.log("Riddle ID:", riddle.id);
     console.log("Riddle Name:", riddle.name);
 
-    // מדידה לדוגמא של זמן לשאלה
+    
     const endTime = time();
     addTimeForAllRidders(returnTime(startTime, endTime));
   };
